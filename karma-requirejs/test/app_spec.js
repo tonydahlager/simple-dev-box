ff7894d83b6ffdace95044ba2809ecdba4883fbd
Tony Dahlager <tonydahlager@gmail.com>
2014-01-18 16:40:47 -0600

    added test arguments to app_spec file

--- karma-requirejs/test/app_spec.js
+++ karma-requirejs/test/app_spec.js
@@ -17,6 +17,25 @@ define(['app', 'jquery', 'lodash'], function(App, $, _) {
 			expect( true ).toEqual(true);
 		});
 		
+		// Test Arguments by Tony
+		
+		it('2 + 2 = 4', function(){
+			var addTwoAndTwo = 2 + 2;
+			expect(addTwoAndTwo).toBe(4);
+		});
+		it('3 is greater than 2', function(){
+			greaterOrLess = function(num1, num2) {
+				if (num1 > num2) {
+					return(num1 + " is greater than " + num2);
+				} else {
+					return(num2 + " is greater than " + num1);
+				}
+			};
+			expect(greaterOrLess(3,2)).toBe('3 is greater than 2');
+		});		
+		it('truth is truth', function(){
+		expect(true).toBe(true);
+		});
 	});
 
 });
